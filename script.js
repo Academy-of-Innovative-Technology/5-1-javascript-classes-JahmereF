0aeb4dfe92303553dee4d1bceea3813bfa41c66b
Jahmere Fernandez <jahmeref2@nycstudents.net>
2026-05-14 12:39:33 -0400

    Part 1, 2, and 3 done

---
+++ script.js
@@ -0,0 +1,26 @@
+class MathFunctions {
+  constructor(a, b) {
+    this.a = a;
+    this.b = b;
+  }
+
+  add() {
+    return this.a + this.b;
+  }
+
+  subtract() {
+    return this.a - this.b;
+  }
+
+  multiply() {
+    return this.a * this.b;
+  }
+
+  divide () {
+    return this.a / this.b;
+  }
+}
+
+const myCalculator = new MathFunctions(10, 5);
+
+console.log(myCalculator.add());
